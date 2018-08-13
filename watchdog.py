281fc2723ef0d562bb694b97899c6081e2f9836c
Z <kerenyi.nagy.zoltan@gmail.com>
2018-08-14 01:07:15 +0200

    Update watchdog.py

--- watchdog.py
+++ watchdog.py
@@ -1,6 +1,5 @@
-from axis import *
-import time
-import csv
+import requests
+import datetime
 import logging
 
 today = datetime.date.today()
@@ -8,20 +7,44 @@ LOG_FORMAT = '%(levelname)s %(asctime)s %(message)s'
 logging.basicConfig(filename='{}.log'.format(today), level=logging.INFO, format=LOG_FORMAT, datefmt='%Y:%m:%d %H:%M:%S')
 logger = logging.getLogger()
 
-freq = 300
+class Axis:
+    """ Axis camera class representing the state by pulling an image from the camera"""
 
-logging.info('[+] Axis Watchdog started, refresh time: {} seconds'.format(freq))
+    def __init__(self, ip='192.168.0.90', username='root', password='root', name='Axis'):
+        self.ip = ip
+        self.username = username
+        self.password = password
+        self.name = name
+        self.base_url = 'http://{}:{}@{}/axis-cgi/'.format(username, password, ip)
+        self.state = None
+        self.get_state()
 
+    def __str__(self):
+        return '{} {} {}'.format(self.name, self.ip, self.state)
 
-while True:
-    try:
-        with open('config.csv', 'r', encoding='utf-8') as csv_file:
-            csv_reader = csv.reader(csv_file)
+    def get_state(self):
+        try:
+            r = requests.get('http://{}:{}@{}/jpg/image.jpg'.format(self.username, self.password, self.ip), timeout=2)
+            if r.status_code == 200:
+                self.state = 'online'
+                logging.info('{} @ online'.format(self.__str__()))
+            elif r.status_code == 404:
+                self.state = 'offline'
+                logging.error('{} @ 404 error'.format(self.__str__()))
+            elif r.status_code == 401:
+                self.state = 'offline'
+                logging.warning('{} @ authentication error'.format(self.__str__()))
+            else:
+                logging.error('{} @ unknown error'.format(self.__str__()))
 
-            for line in csv_reader:
-                a = Axis(ip=str(line[0]).strip(), username=str(line[1]).strip(), password=str(line[2]).strip(), name=str(line[3]).strip())
-    except:
-        logging.error('[-] Axis Watchdog: no config file found')
-        break
+        except:
+            self.state = 'offline'
+            logging.error('{} @ unreachable'.format(self.__str__()))
 
-    time.sleep(freq)
+    def restart(self):
+        try:
+            r = requests.get('{}/restart.cgi'.format(self.base_url))
+            if r.status_code == 200:
+                logging.info('{} @ has been restarted'.format(self.__str__()))
+        except:
+            logging.error('{} @ was unable to restart'.format(self.__str__()))
