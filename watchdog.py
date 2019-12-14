b5e3c6f1c8fb7ace2b5d264d37affde45c4d3a3b
Z <kerenyi.nagy.zoltan@gmail.com>
2019-12-14 21:58:55 +0100

    Update watchdog.py

--- watchdog.py
+++ watchdog.py
@@ -7,6 +7,7 @@ LOG_FORMAT = '%(levelname)s %(asctime)s %(message)s'
 logging.basicConfig(filename='{}.log'.format(today), level=logging.INFO, format=LOG_FORMAT, datefmt='%Y:%m:%d %H:%M:%S')
 logger = logging.getLogger()
 
+
 class Axis:
     """ Axis camera class representing the state by pulling an image from the camera"""
 
@@ -27,24 +28,29 @@ class Axis:
             r = requests.get('http://{}:{}@{}/jpg/image.jpg'.format(self.username, self.password, self.ip), timeout=2)
             if r.status_code == 200:
                 self.state = 'online'
-                logging.info('{} @ online'.format(self.__str__()))
+                logging.info(self.__str__())
             elif r.status_code == 404:
                 self.state = 'offline'
-                logging.error('{} @ 404 error'.format(self.__str__()))
+                logging.error(self.__str__() + ' 404 error')
             elif r.status_code == 401:
                 self.state = 'offline'
-                logging.warning('{} @ authentication error'.format(self.__str__()))
+                logging.warning('{} ,authentication error'.format(self.__str__()))
             else:
-                logging.error('{} @ unknown error'.format(self.__str__()))
+                logging.error('{} ,unknown error'.format(self.__str__()))
 
         except:
             self.state = 'offline'
-            logging.error('{} @ unreachable'.format(self.__str__()))
+            logging.error('{} unreachable'.format(self.__str__()))
 
     def restart(self):
         try:
             r = requests.get('{}/restart.cgi'.format(self.base_url))
             if r.status_code == 200:
-                logging.info('{} @ has been restarted'.format(self.__str__()))
+                logging.info('{} ,has been restarted'.format(self.__str__()))
         except:
-            logging.error('{} @ was unable to restart'.format(self.__str__()))
+            logging.error('{} ,was unable to restart'.format(self.__str__()))
+
+    def restart_if_no_http(self):
+        self.get_state()
+        if self.state == 'offline':
+            self.restart()
